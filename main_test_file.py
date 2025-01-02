7c6718af151508c92b9eca980d4fa5a3cd53fa1f
YusufA442 <Yusuf.Ahmed.6@city.ac.uk>
2025-01-02 16:30:14 +0000

    commit maintests

--- main_test_file.py
+++ main_test_file.py
@@ -1,6 +1,6 @@
-import gauss_rref,gauss_solver
+import gauss_ref,gauss_back_substitution
 
-a=gauss_rref.three_terms([[1,1,-3,6],[2,1,4,3],[5,2,16,4]])
+a=gauss_ref.three_terms([[1,1,-3,6],[2,1,4,3],[5,2,16,4]])
 print(a)
-a=gauss_solver.three_terms([[1,1,-3,6],[2,1,4,3],[5,2,16,4]])
+a=gauss_back_substitution.three_terms([[1,1,-3,6],[2,1,4,3],[5,2,16,4]])
 print(a)
