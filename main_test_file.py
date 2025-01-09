7359d3040c5f67c98a51a6a599300122bf8f3730
YusufA442 <Yusuf.Ahmed.6@city.ac.uk>
2025-01-09 15:42:59 +0000

    some shit
    some shit

--- main_test_file.py
+++ main_test_file.py
@@ -1,6 +1,4 @@
 import gauss_ref,gauss_back_substitution
 
-a=gauss_ref.three_terms([[1,1,-3,6],[2,1,4,3],[5,2,16,4]])
-print(a)
-a=gauss_back_substitution.three_terms([[1,1,-3,6],[2,1,4,3],[5,2,16,4]])
+a=gauss_back_substitution.three_terms([[-1,2,3,5],[3,1,1,10],[1,-1,-1,-2]])
 print(a)
