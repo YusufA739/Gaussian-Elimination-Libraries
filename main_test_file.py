a48038ae7b324e6644c1d78a88a824ca8c3fde76
YusufA442 <Yusuf.Ahmed.6@city.ac.uk>
2025-03-14 19:48:14 +0000

    changed the lib used

--- main_test_file.py
+++ main_test_file.py
@@ -1,4 +1,4 @@
-import gauss_ref,gauss_back_substitution
+import gauss_ref,gauss_reduced_ref
 
-a=gauss_back_substitution.three_terms([[-1,2,3,5],[3,1,1,10],[1,-1,-1,-2]])
+a=gauss_reduced_ref.three_terms([[2,5,10,12],[3,9,2,4],[4,-3,2,-1]])
 print(a)
